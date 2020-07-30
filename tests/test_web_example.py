ba8cb36b77d6472a261a95d88d82c69121d4189f
s.notevskiy <s.notevskiy@gmail.com>
2020-07-30 23:20:21 +0200

    maket for first web test

---
+++ tests/test_web_example.py
@@ -0,0 +1,9 @@
+
+
+def test_search_city_by_full_name(app):
+    """Test searching city by full name"""
+    city_name = "Kaliningrad"
+    # open site
+    # enter query
+    # press search button
+    # check that name in first response
