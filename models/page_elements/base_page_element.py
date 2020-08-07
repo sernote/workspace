97cc33f59cbfea9a24088fea6bfb719f9c34ef3a
s.notevskiy <s.notevskiy@gmail.com>
2020-08-07 23:01:49 +0200

    locators for element creating with dependency from parents

--- models/page_elements/base_page_element.py
+++ models/page_elements/base_page_element.py
@@ -6,10 +6,32 @@ class Element:
     def __init__(self, page: BasePage, name: str, locators: list = None):
         self.page = page
         self.name = f'"{name}"'
-        self.locators = locators
+        self.locators = self.check_for_parents_locators(locators)
         self.app = page.app
         self.wait_timeout = self.page.wait_timeout
 
     def __repr__(self):
         return "%s:%s:%s" % (self.__class__, self.name, self.locators)
 
+    @staticmethod
+    def check_for_parents_locators(locators: list) -> list:
+        """Check locators for parents and build extended one"""
+        for locator in locators:
+            if locator.parent is not None:
+                children_locators_list = locator.parent.generate_children_locators(locator)
+                locators.remove(locator)
+                locators.extend(children_locators_list)
+        return locators
+
+    def generate_children_locators(self, children_locator):
+        """Create list of locators using xpath and all current type locators"""
+        # TODO make smart locator type check
+        target_locator_type = type(self.locators[0])  # this element always created thats why we can use first locator
+        result_locators_list = []
+        children_locator_xpath = children_locator.generate_xpath()
+        for parent_locator in self.locators:
+            if type(children_locator) is target_locator_type:
+                children_locator.locator_value = parent_locator.generate_xpath() + children_locator_xpath
+                children_locator.parent = None
+                result_locators_list.append(children_locator)
+        return result_locators_list
