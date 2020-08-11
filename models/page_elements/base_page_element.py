1f57493de7108a3d65b1a7b3134ffe725af33eed
s.notevskiy <s.notevskiy@gmail.com>
2020-08-12 01:02:34 +0200

    add methods for interact with elements

--- models/page_elements/base_page_element.py
+++ models/page_elements/base_page_element.py
@@ -78,6 +78,48 @@ class Element:
             self.app.log.info('Element is visible')
             return True
 
+    def wait_for_element(self, wait_timeout: int = None, required_visibility: bool = True) -> WebElement:
+        """Waiting for element"""
+        if wait_timeout is None:
+            wait_timeout = self.wait_timeout
+        self.app.log.info(f'Waiting for element {self.name}, page: {self.page.name}')
+        try:
+            if required_visibility is True:
+                element = self.wait_for_visibility_of_any(self.locators, wait_timeout)
+            else:
+                # TODO presense_of_any
+                raise NotImplementedError
+        except exceptions.TimeoutException:
+            self.app.log.error(
+                f'Element {self.name} not found,page: {self.page.name}, time:{wait_timeout} sec.'
+                f' Locators:{self.locators}')
+            raise exceptions.TimeoutException
+        else:
+            return element
+
+    def click(self, required_visibility=True) -> WebElement:
+        """Find and click on element"""
+        result_element = self.wait_for_element(required_visibility=required_visibility, wait_timeout=5)
+        self.app.log.info(f'Clicking on element: {self.name}, page: {self.page.name}')
+        result_element.click()
+        return result_element
+
+    def clear_field(self) -> WebElement:
+        """Find and clearing element(field)"""
+        result_element = self.find_element()
+        self.app.log.info(f'Clearing text in: {self.name}, page: {self.page.name}')
+        result_element.clear()
+        return result_element
+
+    def input_text(self, text) -> WebElement:
+        """Find and input text on element"""
+        result_element = self.wait_for_element(wait_timeout=5)
+        self.app.log.info(f'Sending text in: {self.name}, text: "{text}"')
+        result_element.click()
+        result_element.clear()
+        result_element.send_keys(text)
+        return result_element
+
 
 class VisibilityOfOneFromAny(object):
     """Implement EC visibility_of_any for multi-locators"""
